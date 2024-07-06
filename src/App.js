9513312b6dce46cd7781e65706c2fad1c1d6bf8e
Siphesihle9423 <s.s.s.dlamini9423@gmail.com>
2024-07-06 14:52:22 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 export default function App() {
   return (
   <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
 <footer>
           This project was coded by{" "}
@@ -32,6 +33,7 @@ export default function App() {
             hosted on Netlify
           </a>
         </footer>
+      </div>
       </div>
   );
 }
