cfadc357dd1baa843f02837e62c40bf358828839
Siphesihle9423 <s.s.s.dlamini9423@gmail.com>
2024-07-06 13:54:08 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return (
+  <div className="App">
+    <h1>Weather App</h1>
+<footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Siphesihle Dlamini
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Siphesihle9423/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://coruscating-lolly-0cc63a.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+  );
 }
-
